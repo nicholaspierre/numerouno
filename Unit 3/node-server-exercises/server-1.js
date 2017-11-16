25a3baeb263f28674160cbfeee67e17bed99cb1a
nicholaspierre <nicholaspierre@ac.c4q.nyc>
2017-11-16 17:43:45 -0500

    http module 1 & http module 2

---
+++ Unit 3/node-server-exercises/server-1.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.statusCode = 200;
+  res.write('Hello World\n');
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`Server running at on http://localhost:${port}`);
+});
